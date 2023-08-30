155031a42e930e1f8b270714ac83d51c58a58e85
jktarnoff <justin.tarnoff@gmail.com>
2023-08-30 16:46:19 -0500

    Solved Pangram. One line!

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -1,15 +1,5 @@
 object Pangram {
-    val letterMap: MutableMap<Char, Int> = mutableMapOf()
-
     fun isPangram(input: String): Boolean {
-        input.forEach {
-            letterMap.put(it, letterMap[it]?.plus(1) ?: 1)
-        }
-        for(c in 'a'..'z') {
-            if (letterMap[c] == null || letterMap[c]!!.equals(0)) {
-                return false
-            }
-        }
-        return true
+        return ('a'..'z').all { input.lowercase().contains(it) }
     }
 }
