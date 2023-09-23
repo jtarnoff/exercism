ed5a6076b60c7a70ca4763b3f7a6f89d9c0cdc66
jktarnoff <justin.tarnoff@gmail.com>
2023-09-22 19:51:50 -0500

    Finished rna-transcription

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,9 +1,9 @@
-fun transcribeToRna(dna: String): String = {
-    val map = mapOf(
-        "G" to "C",
-        "C" to "G",
-        "T" to "A",
-        "A" to "U"
+fun transcribeToRna(dna: String): String {
+    val charMap = mapOf(
+        'G' to 'C',
+        'C' to 'G',
+        'T' to 'A',
+        'A' to 'U'
     )
-    return dna.map { map[it] }
+    return dna.map { charMap[it] }.joinToString("")
 }
