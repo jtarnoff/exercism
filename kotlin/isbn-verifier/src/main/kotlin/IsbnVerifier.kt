4c25a06b084ee2c1ff9d139140fe8d2bc7b1b74a
Justin Tarnoff <justin.tarnoff@gmail.com>
2023-10-05 21:56:01 -0500

    Completed ISBN Verification

--- kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
+++ kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
@@ -1,6 +1,13 @@
 class IsbnVerifier {
-
     fun isValid(number: String): Boolean {
-        TODO("Implement this function to complete the task")
+        if (number.filter { it != '-' }.length != 10) return false
+
+        return number.filter { it != '-' }.foldIndexed(0) { index, acc, chr ->
+            when (chr) {
+                'X' -> if (index != 9) return false else 10
+                in '0'..'9' -> chr.digitToInt()
+                else -> return false
+            } * (10 - index)  + acc
+        } % 11 == 0
     }
 }
