e66ee99daff6179b5eb94fad8a04546827ff16da
jktarnoff <justin.tarnoff@gmail.com>
2023-10-07 20:26:14 -0500

    Completed Luhn

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,6 +1,16 @@
 object Luhn {
 
     fun isValid(candidate: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val filtered = candidate.filter { it != ' ' }
+        if (filtered.isEmpty()
+            || filtered.any { !it.isDigit() }
+            || filtered == "0")
+            return false
+
+        return filtered.reversed().mapIndexed { index, chr ->
+            (chr.digitToInt())
+                .let { if (index % 2 == 1) it * 2 else it }
+                .let { if (it >= 10) it - 9 else it }
+        }.sum() % 10 == 0
     }
 }
