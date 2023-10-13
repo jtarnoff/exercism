65a127e440688577e946acdf50c7dc9328a4cda9
jktarnoff <justin.tarnoff@gmail.com>
2023-10-12 19:04:31 -0500

    Finished Transpose, updated Nucleotides, started RomanNumerals

--- kotlin/nucleotide-count/src/main/kotlin/Dna.kt
+++ kotlin/nucleotide-count/src/main/kotlin/Dna.kt
@@ -1,13 +1,9 @@
 class Dna(val input: String) {
-
     init {
         require(Regex("""[AGCT]*""").matches(input))
     }
-
-    val nucleotideCounts: Map<Char, Int>
-        get() {
-            return listOf('A', 'C', 'G', 'T').map {
-                it to input.count { chr -> it == chr }
-            }.toMap()
-        }
+    val nucleotideCounts: Map<Char, Int> =
+        listOf('A', 'C', 'G', 'T').map {
+            it to input.count { chr -> it == chr }
+        }.toMap()
 }
