b6fcab7bc390269aeb343776a48d953b002764c3
jktarnoff <justin.tarnoff@gmail.com>
2023-10-18 12:53:11 -0500

    Set up new exercises series and Bob for tonight.

--- kotlin/transpose/src/main/kotlin/Transpose.kt
+++ kotlin/transpose/src/main/kotlin/Transpose.kt
@@ -1,23 +1,17 @@
-import java.sql.Array
-
 object Transpose {
 
     fun transpose(input: List<String>): List<String> {
         if (input.isEmpty()) return listOf()
-        val numRows = input.maxOf {
-            println("it: $it")
-            println("it.length: ${it.length}")
-            it.length
-        }
-        println("numRows: $numRows")
+        val numRows = input.maxOf { it.length }
 
         val output = Array(numRows) { _ -> "" }
-
         for (i in 0 until numRows) {
-            output[i] = input.fold("") { acc, str -> acc + str.elementAtOrElse(i) { _ -> ' ' } }.take(input.size).trimEnd()
+            output[i] = input.map { it.replace(' ', '%') }
+                .fold("") { acc, str -> acc + str.elementAtOrElse(i) { _ -> ' ' } }
+                .take(input.size)
+                .trimEnd()
+                .replace('%', ' ')
         }
-
-        println("output: $output")
         return output.asList()
     }
 }
