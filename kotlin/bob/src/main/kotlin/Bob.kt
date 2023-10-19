d302dd5f0b7d1bcbcdbafbd9aad2e61ed1386918
jktarnoff <justin.tarnoff@gmail.com>
2023-10-18 19:05:24 -0500

    Complete Series and most of Bob

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,21 @@
 object Bob {
     fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
+        println("input.isQuestion: ${input.isQuestion()}")
+        println("input.isYelling: ${input.isYelling()}")
+        println("input.isSilence: ${input.isSilence()}")
+        return when {
+            input.isQuestion() && input.isYelling() -> "Calm down, I know what I'm doing!"
+            input.isQuestion() -> "Sure."
+            input.isYelling() -> "Whoa, chill out!"
+            input.isSilence() -> "Fine, be that way!"
+            else -> "Whatever."
+        }
     }
+
+    private fun String.isQuestion(): Boolean = this.last() == '?'
+    private fun String.isYelling(): Boolean = this.filter { it.isLetter() }.all { it.isUpperCase() }
+            && this.any { it.isUpperCase() }
+
+    private fun String.isSilence(): Boolean = this.isEmpty() || this.all { it.isWhitespace() }
+
 }
