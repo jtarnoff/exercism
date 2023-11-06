d9bf57d136805d566b68f6b6cbce4f400ed9679d
jktarnoff <justin.tarnoff@gmail.com>
2023-11-05 19:48:10 -0600

    progress on crypto square

--- kotlin/crypto-square/src/main/kotlin/CryptoSquare.kt
+++ kotlin/crypto-square/src/main/kotlin/CryptoSquare.kt
@@ -1,7 +1,42 @@
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
+
 object CryptoSquare {
 
     fun ciphertext(plaintext: String): String {
-        TODO("Implement the function to complete the task")
+        val stripped = plaintext.filter { it.isLetter() || it.isDigit() }
+        val size = stripped.length
+
+        // determine number of rows/cols
+        // x*r + y*c = stripped.length
+        // c >= r
+        // c - r >= 1
+
+        val r = sqrt(size.toDouble()).roundToInt() - 1
+        val c = sqrt(size.toDouble()).roundToInt()
+        println("size: $size")
+        println("r: $r")
+        println("c: $c")
+
+        val chunkedList = stripped.chunked(c)
+        println("chunkedList: $chunkedList")
+
+        val invertedList = chunkedList.mapIndexed { index, _ ->
+            chunkedList.fold("") { acc, s -> acc + s[index].lowercase() }
+        }
+        println("invertedList: $invertedList")
+
+//        (1..invertedList.size).forEachIndexed { index, str ->
+//            invertedList.mapIndexed()
+//        }
+
+//        val newList = invertedList.mapIndexed { index, str ->
+//            invertedList.fold("") { acc, str ->
+//                 acc + str[index]
+//            }
+//        }
+
+        return invertedList.joinToString(" ")
     }
 
 }
