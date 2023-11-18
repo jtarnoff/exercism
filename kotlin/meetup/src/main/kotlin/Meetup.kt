37bda51c40c14eace4e1d5f37e0296c23e556f8d
Justin Tarnoff <justin.tarnoff@gmail.com>
2023-11-17 22:20:27 -0600

    Completed Meetup

--- kotlin/meetup/src/main/kotlin/Meetup.kt
+++ kotlin/meetup/src/main/kotlin/Meetup.kt
@@ -1,11 +1,47 @@
 import java.time.DayOfWeek
 import java.time.LocalDate
 
-class Meetup {
-
-    // TODO: implement proper constructor
+class Meetup(val month: Int, val year: Int) {
+    init {
+        require(month in 1..12)
+    }
 
     fun day(dayOfWeek: DayOfWeek, schedule: MeetupSchedule): LocalDate{
-        TODO("Implement this function to complete the task")
+        val firstDayOfWeek = calculateFirstDayOfWeek(
+            LocalDate.of(year, month, 1).dayOfWeek.value,
+            dayOfWeek.value)
+
+        return when (schedule) {
+            MeetupSchedule.FIRST -> firstDayOfWeek
+            MeetupSchedule.SECOND -> firstDayOfWeek.plusWeeks(1)
+            MeetupSchedule.THIRD -> firstDayOfWeek.plusWeeks(2)
+            MeetupSchedule.FOURTH -> firstDayOfWeek.plusWeeks(3)
+            MeetupSchedule.LAST -> firstDayOfWeek.lastOfMonth()
+            MeetupSchedule.TEENTH -> firstDayOfWeek.teenth()
+        }
+    }
+
+    private fun calculateFirstDayOfWeek(start: Int, end: Int): LocalDate {
+        var diff = end - start
+        if (diff < 0) {
+            diff += 7
+        }
+        return LocalDate.of(year, month, diff + 1)
+    }
+
+    private fun LocalDate.teenth(): LocalDate {
+        var date = this.plusWeeks(1)
+        if (date.dayOfMonth !in 13..19) {
+            return date.plusWeeks(1)
+        }
+        return date
+    }
+
+    private fun LocalDate.lastOfMonth(): LocalDate {
+        var date = this.plusWeeks(3)
+        while (date.dayOfMonth + 7 <= date.month.length(date.isLeapYear)) {
+            date = date.plusWeeks(1)
+        }
+        return date
     }
 }
