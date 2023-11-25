51a5ab5d99ef4898084a091f3c8a38617abad2dd
Justin Tarnoff <justin.tarnoff@gmail.com>
2023-11-25 17:14:38 -0600

    Started RLE exercise

---
+++ kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -0,0 +1,10 @@
+object RunLengthEncoding {
+
+    fun encode(input: String): String {
+        input.sp
+    }
+
+    fun decode(input: String): String {
+        TODO("Implement this to complete the task")
+    }
+}
